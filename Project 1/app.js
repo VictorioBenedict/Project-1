134ac6b0477aad70e49d6fcd614886b2e066ff23
VictorioBenedict <146447773+VictorioBenedict@users.noreply.github.com>
2024-01-24 11:30:44 +0800

    Add files via upload

---
+++ Project 1/app.js
@@ -0,0 +1,148 @@
+const onepiecevid = document.querySelector(".onepiecevid");
+const onepiececlose = document.querySelector(".close");
+const onepiecebutton = document.querySelector(".onepiecebutton");
+
+const narutovid = document.querySelector(".narutovid");
+const narutoclose = document.querySelector(".closetwo");
+const narutobutton = document.querySelector(".narutobutton");
+
+const dragonballzvid = document.querySelector(".dragonballzvid");
+const dragonballzclose = document.querySelector(".closethree");
+const dragonballzbutton = document.querySelector(".dragonballzbutton"); 
+
+const attackontitanvid = document.querySelector(".attackontitanvid");
+const attackontitanclose = document.querySelector(".closefour");
+const attackontitanbutton = document.querySelector(".attackontitanbutton");
+
+const yournamevid = document.querySelector(".yournamevid");
+const yournameclose = document.querySelector(".closefive");
+const yournamebutton = document.querySelector(".yournamebutton");
+
+const iwanttoeatyourpancreasvid = document.querySelector(".iwanttoeatyourpancreasvid");
+const iwanttoeatyourpancreasclose = document.querySelector(".closesix");
+const iwanttoeatyourpancreasbutton = document.querySelector(".iwanttoeatyourpancreasbutton");
+
+
+const angelbeatsvid = document.querySelector(".angelbeatsvid");
+const angelbeatsclose = document.querySelector(".closeseven");
+const angelbeatsbutton = document.querySelector(".angelbeatsbutton");
+
+
+const asilentvoicevid = document.querySelector(".asilentvoicevid");
+const asilentvoiceclose = document.querySelector(".closeeight");
+const asilentvoicebutton = document.querySelector(".asilentvoicebutton");
+
+const deathnotevid = document.querySelector(".deathnotevid");
+const deathnoteclose = document.querySelector(".closenine");
+const deathnotebutton = document.querySelector(".deathnotebutton");
+
+const chainsawmanvid = document.querySelector(".chainsawmanvid");
+const chainsawmanclose = document.querySelector(".closeten");
+const chainsawmanbutton = document.querySelector(".chainsawmanbutton");
+
+const onepunchmanvid = document.querySelector(".onepunchmanvid");
+const onepunchmanclose = document.querySelector(".closeeleven");
+const onepunchmanbutton = document.querySelector(".onepunchmanbutton");
+
+const hunterxhuntervid = document.querySelector(".hunterxhuntervid");
+const hunterxhunterclose = document.querySelector(".closetwelve");
+const hunterxhunterbutton = document.querySelector(".hunterxhunterbutton");
+
+hunterxhunterbutton.addEventListener("click",() =>{
+    hunterxhuntervid.style.display="flex";
+});
+
+hunterxhunterclose.addEventListener("click",() =>{
+    hunterxhuntervid.style.display="none";
+});
+
+onepunchmanbutton.addEventListener("click",() =>{
+    onepunchmanvid.style.display="flex";
+});
+
+onepunchmanclose.addEventListener("click",() =>{
+    onepunchmanvid.style.display="none";
+});
+
+chainsawmanbutton.addEventListener("click",() =>{
+    chainsawmanvid.style.display="flex";
+});
+
+chainsawmanclose.addEventListener("click",() =>{
+    chainsawmanvid.style.display="none";
+});
+
+deathnotebutton.addEventListener("click",() =>{
+    deathnotevid.style.display="flex";
+});
+
+deathnoteclose.addEventListener("click",() =>{
+    deathnotevid.style.display="none";
+});
+
+asilentvoicebutton.addEventListener("click",() =>{
+    asilentvoicevid.style.display="flex";
+});
+
+asilentvoiceclose.addEventListener("click",()=>{
+    asilentvoicevid.style.display="none";
+});
+
+angelbeatsbutton.addEventListener("click",() =>{
+    angelbeatsvid.style.display="flex";
+});
+
+angelbeatsclose.addEventListener("click",()=>{
+    angelbeatsvid.style.display="none";
+});
+
+iwanttoeatyourpancreasbutton.addEventListener("click",()=>{
+    iwanttoeatyourpancreasvid.style.display="flex";
+});
+
+iwanttoeatyourpancreasclose.addEventListener("click",()=>{
+    iwanttoeatyourpancreasvid.style.display="none";
+});
+
+onepiecebutton.addEventListener("click", ()=> {
+    onepiecevid.style.display="flex";
+});
+
+onepiececlose.addEventListener("click",()=>{
+    onepiecevid.style.display="none";
+});
+
+narutobutton.addEventListener("click", ()=> {
+    narutovid.style.display="flex";
+});
+narutoclose.addEventListener("click",()=>{
+    narutovid.style.display="none";
+});
+
+dragonballzbutton.addEventListener("click", ()=> {
+    dragonballzvid.style.display="flex";
+});
+
+dragonballzclose.addEventListener("click",()=>{
+    dragonballzvid.style.display="none";
+});
+
+attackontitanbutton.addEventListener("click",()=>{
+    attackontitanvid.style.display="flex";
+});
+
+attackontitanclose.addEventListener("click",()=>{
+    attackontitanvid.style.display="none";
+});
+
+yournamebutton.addEventListener("click",()=>{
+    yournamevid.style.display="flex";
+});
+yournameclose.addEventListener("click",()=>{
+    yournamevid.style.display="none";
+});
+
+
+
+
+
